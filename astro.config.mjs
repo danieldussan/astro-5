0e66e2ac87228f9e9e748bdb59d87bafcaa59594
Daniel Dussan <daniel2014dussan@gmail.com>
2025-03-20 00:16:12 -0500

    feat: Add Vercel adapter and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,25 @@ import { defineConfig, envField } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+
   output: "server",
+
   env: {
     schema: { 
     SHOW_BUY_BUTTON: envField.boolean({ default: true, context: "server", access: "public" }),
     SCORE_API_ENDPOINT: envField.string({ context: "server", access: "public" })}
   },
+
   experimental: {
     svg: true,
   },
+
+  adapter: vercel(),
 });
